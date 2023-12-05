4cd8934fbe3b44f589252e3c6d63b23b1f1aaba9
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-12-05 21:53:52 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -18,7 +17,6 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -42,13 +40,45 @@ api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+class UpdatePlant(Resource):
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(plant, key, value)
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+
+api.add_resource(UpdatePlant, '/plants/<int:id>')
+
+
+class DeletePlant(Resource):
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204
+
+
+api.add_resource(DeletePlant, '/plants/<int:id>')
 
 
 if __name__ == '__main__':
